4df8bcd3187cfa83b13b745a134d9f0bf766f064
Alexis <thrifthshopdowntheroad@gmail.com>
2020-12-07 23:33:27 -0500

    Deleted a large file

---
+++ Assets/Scenes/ScrollText.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollText : MonoBehaviour
+{
+    public Text text;
+    private string test; 
+    private string[] token;
+
+    void Start(){
+        StartCoroutine(Wait());
+        test = "Tu es l'elu, retrouve le ps4 a dmp";
+    }
+
+
+    void Awake(){
+
+    }
+
+    IEnumerator Wait()
+    {
+        //Print the time of when the function is first called.
+        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+       yield return new WaitForSeconds(1);
+
+        
+        foreach (char r in test){
+        yield return new WaitForSeconds((float)0.2);
+        text.text += r.ToString();
+       }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed;
+    public float jumpForce = 20f;
+    public Rigidbody2D rb;
+    private float mx;
+    public Transform Feet;
+    public LayerMask groundLayers;
+    public Animator anim;
+
+     private void Update()
+    {
+        mx = Input.GetAxisRaw("Horizontal");
+        
+        if (Input.GetButtonDown("Jump") && IsGrounded()) {
+            Jump();
+        }
+
+        if (Mathf.Abs(mx) > 0.05f){
+            anim.SetBool("IsRunning", true);
+        } else {
+            anim.SetBool("IsRunning", false);
+        }
+
+        anim.SetBool("IsGrounded", IsGrounded());
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 movement = new Vector2(mx * movementSpeed, rb.velocity.y);
+
+        rb.velocity = movement;
+    }
+
+    void Jump () {
+        Vector2 movement = new Vector2(rb.velocity.x, jumpForce);
+        rb.velocity = movement;
+    }
+
+    public bool IsGrounded() {
+        Collider2D groundCheck = Physics2D.OverlapCircle(Feet.position, 0.5f, groundLayers);
+
+        if (groundCheck) {
+            return true;
+        } 
+        return false;
+    }
+
+}
